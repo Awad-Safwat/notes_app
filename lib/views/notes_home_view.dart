8ad404404e4470b4ed1e4752eb515e2ca0456e19
Awad-Safwat <80320059+Awad-Safwat@users.noreply.github.com>
2023-08-08 02:49:11 +0300

    Create NotesHomeView

---
+++ lib/views/notes_home_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NotesHomeView extends StatelessWidget {
+  const NotesHomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
