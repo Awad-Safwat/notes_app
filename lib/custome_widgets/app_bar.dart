267afe78ccb6db6e82086ccdee8a07bb82a820ae
Awad-Safwat <80320059+Awad-Safwat@users.noreply.github.com>
2023-08-10 04:43:06 +0300

    not important

--- lib/custome_widgets/app_bar.dart
+++ lib/custome_widgets/app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/custome_widgets/search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
@@ -17,7 +16,7 @@ class CustomAppBar extends StatelessWidget {
           ),
         ),
         Spacer(),
-        CustomSearchIcon(),
+       
       ],
     );
   }
