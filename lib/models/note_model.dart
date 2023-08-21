35bb24df2f6b52f7d798fca394c7750a7a88b0db
Awad-Safwat <awad.safwat1999@gmail.com>
2023-08-22 01:15:15 +0300

    generated haive typeAdapter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   String title;
+  @HiveField(1)
   String supTitle;
+  @HiveField(2)
   String date;
+  @HiveField(3)
   int Color;
 
   NoteModel({
