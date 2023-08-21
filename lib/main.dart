1aebcdbf36b5f58b25fb6c571cdf868b7e648acf
Awad-Safwat <awad.safwat1999@gmail.com>
2023-08-22 01:22:21 +0300

    init hive package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/notes_home_view.dart';
 
-void main() {
+import 'constatns.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyNotsApp());
 }
 
